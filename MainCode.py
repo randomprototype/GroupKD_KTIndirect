33cf2b805701299609a2870e43be5beff012d867
randomprototype <100228373+randomprototype@users.noreply.github.com>
2024-12-18 11:21:15 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -70,8 +70,11 @@ def main():
 
         for i, col in enumerate(columns):
             col.write(f"**Component {i+1}:**")
-            Eta1s.append(col.number_input(f"\u03B7¹", min_value=0.0, value=3.0, key=f"C^I_{i}", help="This parameter represents the characteristic life of the defect arrival for weak component i."))
-            Beta1s.append(col.number_input(f"\u03B2¹", min_value=1.0, max_value=5.0, value=3.0, key=f"C^I_{i}", help="This parameter represents the shape parameter of the defect arrival for weak component i."))
+            Eta1s.append(col.number_input(f"\u03B7¹", min_value=0.0, value=3.0, key=f"Eta1_{i}", help="This parameter represents the characteristic life of the defect arrival for weak component i."))
+            Beta1s.append(col.number_input(f"\u03B2¹", min_value=1.0, max_value=5.0, value=3.0, key=f"Beta1_{i}", help="This parameter represents the shape parameter of the defect arrival for weak component i."))
+            Eta2s.append(col.number_input(f"\u03B7²", min_value=0.0, value=18.0, key=f"Eta2_{i}", help="This parameter represents the characteristic life of the defect arrival for strong component i."))
+            Beta2s.append(col.number_input(f"\u03B2¹", min_value=1.0, max_value=5.0, value=3.0, key=f"Beta2_{i}", help="This parameter represents the shape parameter of the defect arrival for strong component i."))
+            
             Cis.append(col.number_input(f"Inspection Cost", min_value=0.0, value=0.05, key=f"C^I_{i}", help="This parameter represents the cost of conducting an inspection on component i."))
             Crs.append(col.number_input(f"Replacement Cost", min_value=0.0, value=1.0, key=f"C^R_{i}", help="This parameter represents the cost of conducting a replacement (during inspection or at the age-based action) on component i."))
             Cfs.append(col.number_input(f"Failure Cost", min_value=0.0, value=10.0, key=f"C^F_{i}", help="This parameter represents the cost of conducting a replacement during the failure on component i."))
