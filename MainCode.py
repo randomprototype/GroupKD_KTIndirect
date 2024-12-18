501fb2ba7b862ea49c79e8b9ca3579b39bf7dda1
randomprototype <100228373+randomprototype@users.noreply.github.com>
2024-12-18 11:46:00 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -80,7 +80,13 @@ def main():
             Cis.append(col.number_input(f"Inspection Cost", min_value=0.0, value=0.05, key=f"C^I_{i}", help="This parameter represents the cost of conducting an inspection on component i."))
             Crs.append(col.number_input(f"Replacement Cost", min_value=0.0, value=1.0, key=f"C^R_{i}", help="This parameter represents the cost of conducting a replacement (during inspection or at the age-based action) on component i."))
             Cfs.append(col.number_input(f"Failure Cost", min_value=0.0, value=10.0, key=f"C^F_{i}", help="This parameter represents the cost of conducting a replacement during the failure on component i."))
-            st.subheader("Insert the policy:")
+            # Adicionando o campo para inserir os momentos das ações para cada componente
+            moments_input = col.text_input(f"Insert moments for component {i+1}:", key=f"moments_{i}", help="Enter the moments of actions as a comma-separated list.")
+            
+            # Salvando os momentos de ação para cada componente
+            if moments_input:
+                moments_list = [float(x.strip()) for x in moments_input.split(',')]  # Converte para lista de números
+                st.write(f"Moments for component {i+1}: {moments_list}")
         
         col1, col2 = st.columns(2)
 
