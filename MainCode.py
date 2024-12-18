91ad5cf15bba4cb6a7632b37d5012e1f48872afb
randomprototype <100228373+randomprototype@users.noreply.github.com>
2024-12-18 11:55:48 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -84,6 +84,7 @@ def main():
         Si=st.number_input("Insert the setup cost for an inspection", min_value = 0.0, value = 0.07, help="This parameter specifies the common cost for all components when one or more inspections are carried out.")
         Sr=st.number_input("Insert the setup cost for an replacement", min_value = 0.0, value = 0.7, help="This parameter specifies the common cost for all components when one or more replacements are carried out (preventively or correctively).")
         
+        col1, col2 = st.columns(2)
         st.subheader("Insert the policy:")
 
         for i, col in enumerate(columns):
